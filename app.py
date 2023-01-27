038ab50901afa39cb56c3df0df634645799fd92b
Myeongwon Choi <auspic7@gmail.com>
2023-01-28 01:51:11 +0900

    Implement get total asset

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import pykis
 import yfinance as yf
 import pandas as pd
 from allocation import baa_aggressive
-from config import APP_KEY, APP_SECRET, CANO, ACNT_PRDT_CD
+from config import APP_KEY, APP_SECRET, CANO, ACNT_PRDT_CD, VIRTUAL
 from datetime import datetime
+from kis import get_cash_balance
 
 market_codes = {'SPY': "AMS", 'EFA': "AMS", 'EEM': "AMS", 'AGG': "AMS", 'QQQ': "NAS", 'EEM': "AMS", 'EFA': "AMS", 'AGG': "AMS", 'TIP': "AMS", 'DBC': "AMS", 'BIL': "AMS", 'IEF': "NAS", 'TLT': "NAS", 'LQD': "AMS", 'AGG': "AMS", "TSLA": "NAS"}
 
@@ -18,13 +19,22 @@ account_info = {  # 사용할 계좌 정보
 }
 
 # API 객체 생성
-domain_info = pykis.DomainInfo(kind="virtual")
+domain_info = pykis.DomainInfo(kind="virtual" if VIRTUAL else "real")
 api = pykis.Api(domain_info=domain_info, key_info=key_info,
                 account_info=account_info)
 stocks_os = api.get_os_stock_balance()
 print(stocks_os)
 
-asset_usd = 1000
+# 현재 총 자산을 파악
+stock_usd = 0
+if len(stocks_os) > 0:
+    stocks_os.loc[:, "총액"] = stocks_os.loc[:, "현재가"] * stocks_os.loc[:, "보유수량"]
+    stock_usd = stocks_os.loc[:, "총액"].sum()
+cash_usd = get_cash_balance(api)
+asset_usd = stock_usd + cash_usd
+print("현재 총 자산: ", asset_usd)
+
+# 자산배분 비율을 파악
 allocation_ratio = baa_aggressive(datetime.now())
 
 # 현재 얼로케이션을 파악
@@ -34,14 +44,14 @@ current_allocation = api.get_os_stock_balance().loc[:,['보유수량']]
 allocation_goal = pd.DataFrame(data=
     {"보유수량": [allocation_ratio[stock] * asset_usd // api.get_os_current_price(stock, market_codes[stock]) for stock in allocation_ratio.index], "목표비중": allocation_ratio.values}, index=allocation_ratio.index)
 
-diff = allocation_goal.sub(current_allocation, fill_value=0)
+diff = allocation_goal.sub(current_allocation, fill_value=0).rename(columns={"보유수량": "매매계획수량"})
 print(diff)
 
 # 매수/매도
-for stock in diff.loc[diff.loc[:,"보유수량"] < 0].index:
-    api.sell_os_stock(market_codes[stock], stock, int(abs(diff.loc[stock, "보유수량"])), round(api.get_os_current_price(stock, market_codes[stock]) * 0.99, 2))
-for stock in diff.loc[diff.loc[:,"보유수량"] > 0].index:
-    api.buy_os_stock(market_codes[stock], stock, int(abs(diff.loc[stock, "보유수량"])), round(api.get_os_current_price(stock, market_codes[stock]) * 1.01, 2))
+for stock in diff.loc[diff.loc[:,"매매계획수량"] < 0].index:
+    api.sell_os_stock(market_codes[stock], stock, int(abs(diff.loc[stock, "매매계획수량"])), round(api.get_os_current_price(stock, market_codes[stock]) * 0.99, 2))
+for stock in diff.loc[diff.loc[:,"매매계획수량"] > 0].index:
+    api.buy_os_stock(market_codes[stock], stock, int(abs(diff.loc[stock, "매매계획수량"])), round(api.get_os_current_price(stock, market_codes[stock]) * 1.01, 2))
 
 stocks_os = api.get_os_stock_balance()
 print(stocks_os)
--- kis.py
+++ kis.py
@@ -1,15 +1,79 @@
-import requests, json
-from config import URL_BASE, APP_KEY, APP_SECRET
+import pykis
+from pykis import APIRequestParameter
 
-def hashkey(datas):
-  PATH = "uapi/hashkey"
-  URL = f"{URL_BASE}/{PATH}"
-  headers = {
-    'content-Type' : 'application/json',
-    'appKey' : APP_KEY,
-    'appSecret' : APP_SECRET,
-    }
-  res = requests.post(URL, headers=headers, data=json.dumps(datas))
-  hashkey = res.json()["HASH"]
+def get_cash_balance(api):
+  """
+  구매 가능 현금(달러) 조회
+  return: 해당 계좌의 구매 가능한 현금(달러)
+  """
+  url_path = "/uapi/overseas-stock/v1/trading/inquire-present-balance"
+  is_virtual = api.domain.is_virtual()
+  tr_id = "VTRP6504R" if is_virtual else "CTRP6504R"
 
-  return hashkey
+  if api.account is None:
+      msg = "계좌가 설정되지 않았습니다. set_account를 통해 계좌 정보를 설정해주세요."
+      raise RuntimeError(msg)
+
+  params = {
+      "CANO": api.account.account_code,
+      "ACNT_PRDT_CD": api.account.product_code,
+      "WCRC_FRCR_DVSN_CD": "00",
+      "NATN_CD": "840",
+      "TR_MKET_CD": "00",
+      "INQR_DVSN_CD": "00",
+  }
+
+  req = APIRequestParameter(url_path, tr_id, params)
+  res = api._send_get_request(req)
+
+  return float(res.outputs[1][0]['frcr_dncl_amt_2']) if len(res.outputs[1]) > 0 else 0
+
+# import pykis
+# import yfinance as yf
+# import pandas as pd
+# from allocation import baa_aggressive
+# from config import APP_KEY, APP_SECRET, CANO, ACNT_PRDT_CD, VIRTUAL
+# from datetime import datetime
+
+# market_codes = {'SPY': "AMS", 'EFA': "AMS", 'EEM': "AMS", 'AGG': "AMS", 'QQQ': "NAS", 'EEM': "AMS", 'EFA': "AMS", 'AGG': "AMS",
+#                 'TIP': "AMS", 'DBC': "AMS", 'BIL': "AMS", 'IEF': "NAS", 'TLT': "NAS", 'LQD': "AMS", 'AGG': "AMS", "TSLA": "NAS"}
+
+# key_info = {		# KIS Developers 서비스 신청을 통해 발급받은 API key 정보
+#     "appkey": APP_KEY,
+#     "appsecret": APP_SECRET
+# }
+
+# account_info = {  # 사용할 계좌 정보
+#     "account_code": CANO,
+#     "product_code": ACNT_PRDT_CD
+# }
+
+# # API 객체 생성
+# domain_info = pykis.DomainInfo(kind="real" if not VIRTUAL else "virtual")
+# api = pykis.Api(domain_info=domain_info, key_info=key_info,
+#                 account_info=account_info)
+# stocks_os = api.get_os_stock_balance()
+
+# url_path = "/uapi/overseas-stock/v1/trading/inquire-present-balance"
+# is_virtual = api.domain.is_virtual()
+# tr_id = "VTRP6504R" if is_virtual else "CTRP6504R"
+
+# if api.account is None:
+#     msg = "계좌가 설정되지 않았습니다. set_account를 통해 계좌 정보를 설정해주세요."
+#     raise RuntimeError(msg)
+
+# stock_code = ""
+# qry_price = 0
+
+# params = {
+#     "CANO": api.account.account_code,
+#     "ACNT_PRDT_CD": api.account.product_code,
+#     "WCRC_FRCR_DVSN_CD": "00",
+#     "NATN_CD": "840",
+#     "TR_MKET_CD": "00",
+#     "INQR_DVSN_CD": "00",
+# }
+
+# req = APIRequestParameter(url_path, tr_id, params)
+# res = api._send_get_request(req)
+# print(res.outputs[1][0]['frcr_dncl_amt_2'])
